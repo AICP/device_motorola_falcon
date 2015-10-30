402f1bbf3820ae534c1c5cf68c8c4a61888858a4
desaishivam26 <kane.desai@gmail.com>
2015-10-30 20:00:52 +0100

    Use 4.9 toolchains
    Change-Id: Ie35024164c3d86975c89fa47c4f834f6e468e261

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,13 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Kernel
 TARGET_KERNEL_CONFIG := falcon_defconfig
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_falcon.cpp
