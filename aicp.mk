e707fb3af5fa032e0e6e99172d66fbae462618ea
desaishivam26 <kane.desai@gmail.com>
2015-12-19 20:41:23 +0530

    falcon: remove aicp.mk
    Change-Id: I925c8d1d69a2842da7d1915de20640fa57800675

--- aicp.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-# Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
-
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := aicp_falcon
-
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
